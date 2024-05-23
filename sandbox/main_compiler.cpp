5628bfdf04e1ff149ffe3370d02a1ea34cca429e
Ivan Ogorodov <iogorodov@outlook.com>
2024-05-23 21:20:48 +0300

    Cleanup code a little

--- sandbox/main_compiler.cpp
+++ sandbox/main_compiler.cpp
@@ -16,80 +16,10 @@ struct TestScript
 
 int main()
 {
-    auto script_core = std::make_shared<as::Core>();
-    // auto ivnscript_language = std::make_shared<as::IvnScriptLanguage>();
-    const auto test_1_is = script_core->newScriptModule<TestScript>("scripts/test_script.is");
-    const auto test_1_is_instance1(test_1_is->newInstance());
-    assert(test_1_is_instance1->foo(10, 20) == 30);
-
-    // auto s = new ScriptObject(&______sandbox_scripts_test_1_is_13297541326829458225);
-    // auto t = static_cast<TestScript*>(static_cast<void*>(s));
-    // assert(t->foo(10, 20) == 30);
-    // assert(t->bar(10) == 100);
-
-    // assert(______sandbox_scripts_test_1_is_13297541326829458225.bar(nullptr, 10) == 100);
-    // ScriptObject* s = new ScriptObject(______sandbox_scripts_test_1_is_13297541326829458225);
-    // TestScript* t = static_cast<TestScript*>(static_cast<void*>(s));
-    // t->foo(10, 20);
-
-    // auto script_core = std::make_shared<as::Core>();
-    // auto lua_language = std::make_shared<as::LuaLanguage>();
-    // auto squirrel_language = std::make_shared<as::SquirrelLanguage>();
-    // auto ivnscript_language = std::make_shared<as::IvnScriptLanguage>();
-    //
-    // script_core->registerLanguage("lua", std::move(lua_language));
-    // script_core->registerLanguage("nut", std::move(squirrel_language));
-    // script_core->registerLanguage("is", std::move(ivnscript_language));
-
-    // const auto test_1_lua = script_core->newScriptModule<TestScript>("../../sandbox/scripts/test_1.lua");
-    // const auto test_1_nut = script_core->newScriptModule<TestScript>("../../sandbox/scripts/test_1.nut");
-    // const auto test_1_is = script_core->newScriptModule<TestScript>("../../sandbox/scripts/test_1.is");
-
-    // const auto test_1_lua_instance1(test_1_lua->newInstance());
-    // const auto test_1_lua_instance2(test_1_lua->newInstance());
-    //
-    // assert(test_1_lua_instance1->foo(10, 20) == 30);
-    // assert(test_1_lua_instance1->foo(10, 20) == 30);
-    // assert(test_1_lua_instance1->bar(10) == 1000);
-    // assert(test_1_lua_instance1->bar(20) == 2000);
-    // assert(test_1_lua_instance2->foo(10, 20) == 30);
-    // assert(test_1_lua_instance2->foo(10, 20) == 30);
-    // assert(test_1_lua_instance2->bar(10) == 1000);
-    // assert(test_1_lua_instance2->bar(20) == 2000);
-
-//********************************************************************************************************************//
-
-    // const auto test_1_nut_instance1(test_1_nut->newInstance());
-    // const auto test_1_nut_instance2(test_1_nut->newInstance());
-    //
-    // assert(test_1_nut_instance1->foo(10, 20) == 100031);
-    // assert(test_1_nut_instance1->foo(10, 20) == 100032);
-    //
-    // assert(test_1_nut_instance1->bar(10) == 1000000);
-    // assert(test_1_nut_instance1->bar(20) == 2000000);
-    //
-    // assert(test_1_nut_instance2->foo(10, 20) == 100033);
-    // assert(test_1_nut_instance2->foo(10, 20) == 100034);
-    //
-    // assert(test_1_nut_instance2->bar(10) == 1000000);
-    // assert(test_1_nut_instance2->bar(20) == 2000000);
-
-//********************************************************************************************************************//
-
-    // const auto test_1_is_instance1(test_1_is->newInstance());
-    // const auto test_1_is_instance2(test_1_is->newInstance());
-    //
-    // assert(test_1_is_instance1->foo(10, 20) == 20);
-    // assert(test_1_is_instance1->foo(10, 20) == 20);
-    //
-    // assert(test_1_is_instance1->bar(10) == 100);
-    // assert(test_1_is_instance1->bar(20) == 200);
-    //
-    // assert(test_1_is_instance2->foo(10, 20) == 20);
-    // assert(test_1_is_instance2->foo(10, 20) == 20);
-    //
-    // assert(test_1_is_instance2->bar(10) == 100);
-    // assert(test_1_is_instance2->bar(20) == 200);
+    auto core = std::make_shared<as::Core>();
+    const auto module = core->newScriptModule<TestScript>("scripts/test_script.is");
+    const auto instance(module->newInstance());
+    assert(instance->foo(10, 20) == 30);
 
     return 0;
 }
