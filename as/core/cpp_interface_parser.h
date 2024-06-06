b3fa62b3e35df1a8892f9b52269c726c24f4b8ce
Ivan Ogorodov <iogorodov@outlook.com>
2024-06-06 22:25:55 +0300

    Move ScriptInterface to separate file

--- as/core/cpp_interface_parser.h
+++ as/core/cpp_interface_parser.h
@@ -13,6 +13,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "script_interface.h"
+
 namespace llvm
 {
   class FunctionType;
@@ -34,24 +36,6 @@ namespace clang
 
 namespace as
 {
-
-struct ScriptInterface
-{
-  // Name of interface
-  std::string name;
-  // Ordered vector of method names, if some name is empty it means that method is not pure
-  std::vector<std::string> methodNames;
-  // Types if method since vtable_t contains pointers to methods
-  std::vector<llvm::FunctionType*> methodTypes;
-  // Type of script interface
-  llvm::StructType* interface_t = nullptr;
-  // Type of pointer to script interface
-  llvm::PointerType* interface_ptr_t = nullptr;
-  // Type of vtable for script interface
-  llvm::StructType* vtable_t = nullptr;
-
-  void dump(llvm::raw_fd_ostream& stream);
-};
 
 using ScriptInterfaces = std::unordered_map <std::string, std::shared_ptr<ScriptInterface>>;
 
