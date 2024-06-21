0ce6ce2cbf7f553baa6c3102deda46363217dc29
Ivan Ogorodov <iogorodov@outlook.com>
2024-06-21 13:09:18 +0300

    Tests do not rely on working directory

--- test/features_test_fixture.h
+++ test/features_test_fixture.h
@@ -5,36 +5,28 @@
 #pragma once
 
 #include <gtest/gtest.h>
+#include <mlir/Support/InterfaceSupport.h>
 
 #include "as/core/core.h"
 #include "as/core/language.h"
 #include "as/core/language_runtime.h"
 
+template <typename T> const char* getHeaderFileName();
+
 class FeaturesTestFixture : public testing::Test {
-public:
+protected:
     enum class TreatDouble : unsigned short {
         AsDouble = 0,
         AsFloat = 1,
         AsInteger = 2
     };
 
-protected:
-    FeaturesTestFixture();
+    FeaturesTestFixture() = default;
     ~FeaturesTestFixture() override;
 
     virtual const char* getLanguageName() const = 0;
     virtual std::shared_ptr<as::ILanguage> createLanguage() const = 0;
     virtual std::shared_ptr<as::ILanguageRuntime> createRuntime() const = 0;
-
-    void SetUp() override;
-
-    as::Core& getCore()
-    {
-        return *m_core;
-    }
-
-    std::string copyFile(const std::string& source_file, const std::string& dest_file);
-    std::string createFile(const std::string& content, const std::string& dest_file);
 
     void doSimpleTest(const char* code);
     void doIntegerTest(const char* code);
@@ -47,4 +39,18 @@ protected:
 private:
     std::unique_ptr<as::Core> m_core;
     std::unordered_set<std::filesystem::path> m_temp_files;
+
+    as::Core& ensureCore();
+
+    bool writeFile(const std::string& file_name, const std::string& content);
+
+    template<typename Interface>
+    bool writeHeader()
+    {
+        return writeHeader(getHeaderFileName<Interface>(), Interface::getSourceCode());
+    }
+
+    bool writeHeader(const std::string& file_name, const std::string& code);
+
+    std::string writeCode(const std::string& file_name, const std::string& code);
 };
